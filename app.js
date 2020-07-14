2482f3f5b14073932e8611c02413fcf12b1cf514
myfra <myfragithub@gmail.com>
2020-07-14 10:59:53 +0700

    koneksi database

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var db = require('./config/db');
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ config/db.js
@@ -0,0 +1,17 @@
+require('dotenv/config');
+var mysql = require('mysql');
+
+
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "maturnuwun",
+    password: "maturnuwun",
+    database: "website_design",
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+module.exports = db;
